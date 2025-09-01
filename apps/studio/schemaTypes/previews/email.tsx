95f45e657543115e684d293e7bb13c09277188b3
ChrisLaRocque <larocque.christopher@gmail.com>
2025-09-01 19:47:23 -0400

    Update email.tsx

--- apps/studio/schemaTypes/previews/email.tsx
+++ apps/studio/schemaTypes/previews/email.tsx
@@ -1,16 +1,28 @@
-import {useCallback, useEffect, useState} from 'react'
+import {useCallback, useEffect, useRef, useState} from 'react'
 import {StripeWelcomeEmail} from 'email'
 import {render} from '@react-email/render'
 
 export const EmailPreview = ({document}: {document: any}) => {
   const [bodyHtml, setBodyHtml] = useState('')
+  const debounceRef = useRef<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
-    const renderHtml = async () => {
-      const yes = await render(<StripeWelcomeEmail {...document?.displayed} />)
-      setBodyHtml(yes)
+    if (debounceRef.current) {
+      clearTimeout(debounceRef.current)
     }
-    renderHtml()
+    debounceRef.current = setTimeout(() => {
+      const renderHtml = async () => {
+        const yes = await render(<StripeWelcomeEmail {...document?.displayed} />)
+        setBodyHtml(yes)
+      }
+      renderHtml()
+    }, 300) // 300ms debounce
+    return () => {
+      if (debounceRef.current) {
+        clearTimeout(debounceRef.current)
+      }
+    }
   }, [document.displayed])
+
   return <div dangerouslySetInnerHTML={{__html: bodyHtml}} />
 }
